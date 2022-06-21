36d58684ede05514d469b3944c0f2c2c79f07a0e
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-06-20 21:46:08 -0500

    avance nuevo

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+//import pokemon from '../data/pokemon/pokemon.js';
+//console.log(pokemon);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -14,12 +14,35 @@
 //   .catch(console.error);
 //
 
-const App = () => {
+
+import webdev from '../data/webdev/webdev.js';
+console.log(webdev.items);
+
+  const App = () => {
   const el = document.createElement('div');
 
   el.className = 'App';
   el.textContent = 'Hola mundo!';
 
+ const board = document.createElement('div')
+
+ board.className = 'board'
+ el.appendChild(board)
+
+
+ let items = webdev.items
+ for(let i=0; i<items.length; i++){
+  let iconos = items[i].image
+  let iconoCards = document.createElement('img');
+  iconoCards.className = 'estiloCards'
+  iconoCards.src = iconos
+ 
+  board.appendChild(iconoCards)
+ }
+
+ 
+
+  
   return el;
 };
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 import App from './components/App.js';
 
 document.getElementById('root').appendChild(App());
+
+
+
+git
+
+
+
+
+
+
+
+
+
+
+
+
+
