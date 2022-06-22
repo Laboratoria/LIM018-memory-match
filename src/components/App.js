5a7dddd1df872c29011abedfccd992917cccaf58
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-06-22 14:55:43 -0500

    mostrar cartas

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,9 @@
 // JavasSript que contenga el `export` correspondiente...
 //
 // import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
 
-import webdev from '../data/webdev/webdev.js' // importo la data de webdev
-//console.log(webdev)
-//console.log(webdev.items[0])
-//console.log(webdev.items[0].image)
-
-//----
-let itemss = webdev.items 
-for (let i=0; i<itemss.length; i++) {
-  let imagen = itemss[i].image
-  console.log(imagen)
-    //console.log(items[i])
-}
-//---
-
+// importo la data de webdev
+import webdev from '../data/webdev/webdev.js' 
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -29,39 +16,62 @@ for (let i=0; i<itemss.length; i++) {
 //   .catch(console.error);
 //
 
+
+
 const App = () => {
   const el = document.createElement('div');
 
   el.className = 'App';
   //el.textContent = 'Hola mundo!';
-
-  //---
+  //return el;
   const divCartas = document.createElement('div')
 
   divCartas.className = 'divCartas'
   el.appendChild(divCartas)
 
-  let items = webdev.items
+  let webdevArray = webdev.items
+  let dobleItems = webdevArray.concat(webdevArray)
+
+//console.log(dobleItems)
+
+  dobleItems.forEach(mostrarCartas => {
+    let imagenesCartas = mostrarCartas.image
+    //console.log(imagenesCartas)
+    let divIconos = document.createElement('img')
+    divIconos.className = 'estiloCartas'
+
+    divIconos.src = imagenesCartas
+
+    divCartas.appendChild(divIconos)
+  //console.log(imagenesCartas)
+  
+  });
+
+  //---
+  //const divCartas = document.createElement('div')
+
+  //divCartas.className = 'divCartas'
+  //el.appendChild(divCartas)
+
+  /*let items = webdev.items
   let dobleItems = items.slice()
-  console.log(dobleItems)
+  //console.log(dobleItems)
   for (let i=0; i<items.length; i++) {
     let imagen = items[i].image
+    let dobleImagen = dobleItems[i].image
     //console.log(typeof(imagen))
     let imgCartas = document.createElement('img')
+    let imgDoble = document.createElement('img')
     imgCartas.className = 'estiloCartas'
-    imgCartas.src = imagen  // accedemos a la url de la etiqueta img
+    imgDoble.className = 'estiloCartas'
+    
+    imgCartas.src = imagen // accedemos a la url de la etiqueta img
+    imgDoble.src = dobleImagen
+
     divCartas.appendChild(imgCartas)
-  }
-
-  //let imgCartas = document.createElement('img')
-  //imgCartas.src = items[0].image
- // el.appendChild(imgCartas)
-
-
-
-
+    divCartas.appendChild(imgDoble)
+  }*/
   return el;
 };
-
 
 export default App;
