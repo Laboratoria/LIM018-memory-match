cfd3d7d2716eee2f86816db5310d211a425c59c6
Jackeline Puruaya <jaishackeline@gmail.com>
2022-06-25 16:40:21 -0500

    componente en Html maquetado

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+//import pokemon from '../data/pokemon/pokemon.js';
+//console.log(pokemon);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -15,12 +15,23 @@
 //
 
 const App = () => {
-  const el = document.createElement('div');
+  const end = document.createElement('div');
+  end.className = 'end'
+  const inputName = document.createElement('input');
+  inputName.className = 'inputName';
+  inputName.placeholder = "Escribe tu nombre";
+  const btn_play = document.createElement('button');
+  btn_play.className = 'btn_play';
+  const btn_text = document.createTextNode('Jugar')
+  btn_play.appendChild(btn_text);
+  end.appendChild(inputName);
+  end.appendChild(btn_play);
+ 
+  /* end.className = 'App';
+  end.textContent = 'Soy Jackie!'; */
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
-
-  return el;
+  return end;
+  //return `${inputName}` + `${btn_play}`;
 };
 
 export default App;
