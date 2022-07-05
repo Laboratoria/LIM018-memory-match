3f18aa1c9eaa91fec6922db8fc511300d8fc0d60
jackie2806 <jaishackeline@gmail.com>
2022-07-04 22:25:27 -0500

    función barajar operativa js en Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,11 +5,20 @@ const Data = () => {
   //duplicar cartas
   const myArr = arr.concat(arr);
   //barajar cartas
-  /* function suffle(myArr){
+  function suffle(myArr){
     for (let i = 0; i< myArr.length;i++){
+      let random = Math.floor(Math.random()*myArr.length);
+      let temPosition = "";
+      let currentArray = myArr[i];
+      let randomPosition = myArr[random];
+      temPosition= currentArray;
+      myArr[i]=randomPosition;
+      myArr[random] = temPosition;
 
     } 
-  } */
+    return myArr;
+  } 
+  suffle(myArr);
   //Dibujar cartas
   let card = "";
   let name = "";
@@ -21,7 +30,7 @@ const Data = () => {
     img = myArr[i].image;
     color = myArr[i].bgColor;
     card +=`<div class= "card" style="background-color: ${color}"> <img src="${img}" ><p class= "pokemon_name">${name.charAt(0).toUpperCase()}${name.slice(1)}</p></div>`;
- 
+  
 }
 return card;
     
