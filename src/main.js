3a837230104fbf4f8ab1af5067c7296b4e5613dd
jackie2806 <jaishackeline@gmail.com>
2022-07-15 16:08:30 -0500

    problema con los eventos resuelto

--- src/main.js
+++ src/main.js
@@ -2,54 +2,44 @@ import Start from './components/Start.js';
 import Head from './components/Head.js';
 import App from './components/App.js';
 
-const root = document.getElementById('root'); //hizo la magia-comprendido 
+const root = document.getElementById('root'); //hizo la magia (comprendido) 
 
 root.appendChild(Start());
-root.appendChild(App());
 
-//console.log(Start());
 //innetHTML = ""; borro el contenido (a Root), sobreescribo Data
 //limpiar la página y añadir el componente
 
 const btn_play = root.querySelector('.btn_play');
 const input_name = root.querySelector('.input_name');
 
-//const div_box = document.querySelector('.div_box');
-const card = root.querySelector('.card'); //con querySelectorAll card ya no un elemento, sino un array de elementos 
-
-console.log(card);
+let cards;
 
 btn_play.addEventListener('click', (event) => {
   event.preventDefault();
   if (input_name.value === "") {
     alert('Escribe tu nombre para empezar a jugar');
     //dibujar el modal 
-      
+
 
   } else {
     root.innerHTML = "";
-   // debugger;
+    // debugger;
     root.appendChild(Head(input_name));
     root.appendChild(App());
-    
+    cards = root.querySelectorAll('.card'); //con querySelectorAll card ya no un elemento, sino un array de elementos 
+    cards.forEach(card => {
+      card.addEventListener('click', (e) => {
+        card.classList.toggle('toggleCard');
+        checkCards(e);
+      })
+    })
+
   }
 
-}); 
-//mover cartas
-//debugger;
-card.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('hola soy yo');
-
- card.classList.toggle('toggleCard');
-  checkCards(e); 
-       
 });
 
 
-
-
- //Emparejar cartas
+//Emparejar cartas
 const checkCards = (e) => {
 
   const clicked_cards = e.target;
@@ -58,8 +48,8 @@ const checkCards = (e) => {
   const flipped_cards = document.querySelectorAll('.flipped');
   //const toggle_cards = document.querySelectorAll('div.toggleCard');
   //lógica de validación
-  if(flipped_cards.length === 2){//se valida las 2 cartas elegidas
-    if(flipped_cards[0].getAttribute('id') === flipped_cards[1].getAttribute('id')){
+  if (flipped_cards.length === 2) {//se valida las 2 cartas elegidas
+    if (flipped_cards[0].getAttribute('id') === flipped_cards[1].getAttribute('id')) {
       console.log('match')
       flipped_cards.forEach((card) => {
         card.classList.remove('flipped');
@@ -69,7 +59,7 @@ const checkCards = (e) => {
       console.log('wrong')
       flipped_cards.forEach((card) => {
         card.classList.remove('flipped');
-        setTimeout(() => card.classList.remove('toggleCard'),1000);
+        setTimeout(() => card.classList.remove('toggleCard'), 1000);
       })
     }
   }
