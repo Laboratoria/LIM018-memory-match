cd470d9d89076a4db64d5aaaefff04087adaad45
jackie2806 <jaishackeline@gmail.com>
2022-07-14 22:58:12 -0500

    funcional en cuanto a JS

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-//import Start from './components/Start.js';
-//import Head from './components/Head.js';
+import Start from './components/Start.js';
+import Head from './components/Head.js';
 import App from './components/App.js';
 
 const root = document.getElementById('root'); //hizo la magia (comprendido) 
 
 
-//root.appendChild(Start());
+root.appendChild(Start());
 root.appendChild(App());
 
 //console.log(Start());
 //innetHTML = ""; borro el contenido (a Root), sobreescribo Data
 //limpiar la página y añadir el componente
 
-/* const btn_play = root.querySelector('.btn_play');
+const btn_play = root.querySelector('.btn_play');
 const input_name = root.querySelector('.input_name');
 //console.log(input_name);
 btn_play.addEventListener('click', (event) => {
@@ -27,6 +27,6 @@ btn_play.addEventListener('click', (event) => {
     root.appendChild(App());
   }
 
-}); */
+}); 
  
 
