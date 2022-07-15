07aefc03aab55bb6b19da9e043ff77cac9c2b4cb
jackie2806 <jaishackeline@gmail.com>
2022-07-14 22:50:32 -0500

    limpieza de código

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,8 @@ const App = () => {
   myArr.forEach((item) => {
     const card = document.createElement('div');
     card.classList = 'card';
-    card.setAttribute('id', item.id); 
-      
+    card.setAttribute('id', item.id);
+
     const face = document.createElement('div');
     const img_pokemon = document.createElement('img');
     img_pokemon.classList = 'img_pokemon';
@@ -23,7 +23,7 @@ const App = () => {
     face.classList = 'face';
     img_pokemon.src = item.image;
     face.style.background = item.bgColor;
-    text_pokemon.innerText = item.id.charAt(0).toUpperCase()+item.id.slice(1);
+    text_pokemon.innerText = item.id.charAt(0).toUpperCase() + item.id.slice(1);
     face.appendChild(img_pokemon);
     face.appendChild(text_pokemon);
     face.classList.toggle('toggleCard');
@@ -39,9 +39,9 @@ const App = () => {
       //e.preventDefault();
       card.classList.toggle('toggleCard');
       checkCards(e);
-           
+
     });
-     //Emparejar cartas
+    //Emparejar cartas
     const checkCards = (e) => {
 
       const clicked_cards = e.target;
@@ -50,27 +50,27 @@ const App = () => {
       const flipped_cards = document.querySelectorAll('.flipped');
       //const toggle_cards = document.querySelectorAll('div.toggleCard');
       //lógica de validación
-      if(flipped_cards.length === 2){//se valida las 2 cartas elegidas
-        if(flipped_cards[0].getAttribute('id') === flipped_cards[1].getAttribute('id')){
-          console.log('match')
+      if (flipped_cards.length === 2) {//se valida las 2 cartas elegidas
+        if (flipped_cards[0].getAttribute('id') === flipped_cards[1].getAttribute('id')) {
+          //console.log('match')
           flipped_cards.forEach((card) => {
             card.classList.remove('flipped');
             card.style.pointerEvents = 'none';
           })
         } else {
-          console.log('wrong')
+          //console.log('wrong')
           flipped_cards.forEach((card) => {
             card.classList.remove('flipped');
-            setTimeout(() => card.classList.remove('toggleCard'),1000);
+            setTimeout(() => card.classList.remove('toggleCard'), 1000);
           })
         }
       }
     }
 
-    
+
   });
 
-  
+
   return div_box;
 }
 
--- src/main.js
+++ src/main.js
@@ -2,14 +2,8 @@
 //import Head from './components/Head.js';
 import App from './components/App.js';
 
-const root = document.getElementById('root'); //hizo la magia-comprendido 
+const root = document.getElementById('root'); //hizo la magia (comprendido) 
 
-//consitas que aprendí con David Granados = variable espacio de memoria en el navegador
-//Es estratégico guardar nodos dentro de una variable... pues cuando borramos el contenido con innerHTML = "" (se borra todo lo que se escribe en el DOM, pero no que se almacena en las variables)
-//También aprendí debbuger con la consola (que son como los breakpoint), cuando está en verde esa línea no se ha ejecutado aún (debagger).
-//puedo crear las ramas para hacer cambios e ir probando 
-//para salir de una rama solo es necesario hacer git commit 
-//para borrar una rama: git branch -D prueba
 
 //root.appendChild(Start());
 root.appendChild(App());
